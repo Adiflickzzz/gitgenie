7cea3ad09c6342be261a0f5551d97b9d271061d2
S.Adithya Vardhan <168300867+Adiflickzzz@users.noreply.github.com>
2024-12-22 11:31:40 +0530

    be & fe/commits

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,12 +1,51 @@
 "use client";
+import useProject from "@/hooks/use-project";
 import { useUser } from "@clerk/nextjs";
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
 import React from "react";
+import CommitLog from "./commit-log";
 
 type Props = {};
 
 const DashboardPage = (props: Props) => {
-  const { user } = useUser();
-  return <div>{user?.firstName}</div>;
+  const { project } = useProject();
+  return (
+    <div>
+      <div className="flex flex-wrap items-center justify-between gap-y-4">
+        {/* Github link */}
+        <div className="w-fit rounded-md bg-primary px-4 py-3">
+          <div className="flex items-center gap-2">
+            <Github className="size-5 text-white" />
+            <div className="text-sm font-medium text-white">
+              This project it linked to{" "}
+              <Link
+                href={project?.githubUrl ?? ""}
+                className="inline-flex items-center gap-2 text-white/80 hover:underline"
+              >
+                {project?.githubUrl}
+                <ExternalLink className="size-4" />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="h-4"></div>
+
+        <div className="flex items-center gap-4">
+          Team members InviteButton ArchieveButton
+        </div>
+      </div>
+      <div className="mt-4">
+        {/* <div className="grid grid-cols-1 gap-4 sm:grid-cols-5">
+          AskQuestionCard MeetingCard
+        </div> */}
+      </div>
+
+      <div className="mt-8">
+        <CommitLog />
+      </div>
+    </div>
+  );
 };
 
 export default DashboardPage;
--- src/server/api/routers/project.ts
+++ src/server/api/routers/project.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { pollCommits } from "@/lib/github";
+import { error } from "console";
 
 export const projectRouter = createTRPCRouter({
   createProject: protectedProcedure
@@ -22,6 +24,7 @@ export const projectRouter = createTRPCRouter({
           },
         },
       });
+      await pollCommits(project.id);
       return project;
     }),
 
@@ -37,4 +40,16 @@ export const projectRouter = createTRPCRouter({
       },
     });
   }),
+  getCommits: protectedProcedure
+    .input(
+      z.object({
+        projectId: z.string(),
+      }),
+    )
+    .query(async ({ ctx, input }) => {
+      pollCommits(input.projectId).then().catch(error);
+      return await ctx.db.commit.findMany({
+        where: { projectId: input.projectId },
+      });
+    }),
 });
